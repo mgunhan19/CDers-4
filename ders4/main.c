d08e043f32e9124954c34f50b8959b7bcf580a95
Mehmet Günhan <154965833+mgunhan19@users.noreply.github.com>
2024-10-23 16:35:17 +0300

    Add files via upload

---
+++ ders4/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct node *start = NULL;
+struct node *temp = NULL;
+
+void basaEkle(int veri) {
+    struct node *eleman;
+    eleman = (struct node *) malloc(sizeof(struct node));
+    eleman->data = veri;
+    eleman->next = start;
+    start = eleman;
+}
+
+void sonaEkle(int veri) {
+    struct node *eleman;
+    eleman = (struct node *) malloc(sizeof(struct node));
+    eleman->data = veri;
+    eleman->next = NULL;
+
+    if (start == NULL) {
+        start = eleman;
+    } else {
+        temp = start;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = eleman;
+    }
+}
+
+
+
+void yazdir() {
+    temp = start;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int sayi, adet = 0, secim, n;
+
+    while (adet != 6) {
+        printf("\nBasa eleman eklemek icin 1\n");
+        printf("Sona eleman eklemek icin 2\n");
+    
+        printf("Seciminizi yapiniz: ");
+        scanf("%d", &secim);
+
+        switch (secim) {
+            case 1:
+                printf("\nBasa eklenecek elemanin degeri: ");
+                scanf("%d", &sayi);
+                basaEkle(sayi);
+                yazdir();
+                break;
+
+            case 2:
+                printf("\nSona eklenecek elemanin degeri: ");
+                scanf("%d", &sayi);
+                sonaEkle(sayi);
+                yazdir();
+                break;
+
+        
+
+            default:
+                printf("Gecersiz secim!\n");
+                break;
+        }
+        adet++;
+    }
+
+    return 0;
+}
